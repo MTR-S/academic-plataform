b737526f0647be68ea57371b4a54a9d37ef58810
MTR-S <matheusalmeidass14@gmail.com>
2026-06-17 13:04:15 -0300

    tests: Banco de dados H2 na memória para testes

--- services/auth-service/src/test/java/com/edu/academic_plataform/auth_service/AuthServiceApplicationTests.java
+++ services/auth-service/src/test/java/com/edu/academic_plataform/auth_service/AuthServiceApplicationTests.java
@@ -2,12 +2,15 @@ package com.edu.academic_plataform.auth_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AuthServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Usa o H2 na memória do GitHub
 	}
 
 }
